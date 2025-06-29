c8dfa402aff93f1df9e52c9b068cbfbf0ed92e5c
ekkx <geniusnaru@gmail.com>
2025-06-29 17:07:37 +0900

    feat: add login page

--- web/app/routes.ts
+++ web/app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
+  route("/login", "./routes/login.tsx"),
+
   layout("./routes/layout.tsx", [
     route("/home", "./routes/home.tsx"),
     route("/profile", "./routes/profile.tsx"),
