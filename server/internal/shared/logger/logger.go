1c32ce4ad8b2235594e4ff471e2a4d0404bff8e7
ekkx <geniusnaru@gmail.com>
2025-07-01 07:47:13 +0000

    feat: add logger packge

---
+++ server/internal/shared/logger/logger.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/ekkx/tcmrsv-web/server/internal/config"
+	"github.com/lmittmann/tint"
+)
+
+func parseLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
+
+func Init(cfg *config.Config) {
+	var handler slog.Handler
+
+	switch cfg.Env {
+	case config.EnvDevelopment:
+		handler = tint.NewHandler(os.Stdout, &tint.Options{
+			Level:      parseLevel(cfg.Log.Level),
+			TimeFormat: "15:04:05.000",
+		})
+	default:
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: parseLevel(cfg.Log.Level),
+		})
+	}
+
+	slog.SetDefault(slog.New(handler))
+}
